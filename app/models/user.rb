cfd5dbbef1c07ee909ad06f737432bb1df539ef9
rachelbookhout <rbookhou@gmail.com>
2014-11-02 20:05:41 -0500

    Added Devise

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+    has_many :favorites
+    has_many :recipes, through: :favorites
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
