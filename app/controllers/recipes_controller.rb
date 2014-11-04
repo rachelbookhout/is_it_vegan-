e0e822571eda80088151d87aefc39cd53ed4b357
rachelbookhout <rbookhou@gmail.com>
2014-11-04 15:49:33 -0500

    Switched to gem

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,18 @@
 class RecipesController < ApplicationController
   def index
-
+    #search
+    #@recipes
+#     require 'net/http'
+#     results = Net::HTTP.get("http://api.yummly.com/v1/api/recipes?_app_id=ENV["YUMMLY_APPLICATION_ID"]&_app_key=ENV["YUMMLY_APPLICATION_KEY"]&q=#{search}
+# ")
+#     @recipes = results.parse
   end
 
+  def show
+
+    #@recipe
+  end
 end
+
+
+
---
+++ config/initializers/yummly.rb
@@ -0,0 +1,5 @@
+Yummly.configure do |config|
+  config.app_id = "834b4e5b"
+  config.app_key = "28dfa115a69e48284a2de0381780f28c"
+  config.use_ssl = true # Default is false
+end
