324fdbaf08894c353a242f35254d9c589ba7c7d0
rachelbookhout <rbookhou@gmail.com>
2014-11-05 10:37:32 -0500

    Playing around with using the API again

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,18 @@
 class RecipesController < ApplicationController
   require 'json'
+  require 'net/http'
+
   def index
+    app_id = ENV["YUMMLY_APPLICATION_ID"]
+    app_key = ENV["YUMMLY_APPLICATION_KEY"]
     @search = params[:search]
     if @search
-      binding.pry
       @recipes = Yummly.search(params[:search])
-      binding.pry
-    end
+      url = "http://api.yummly.com/v1/api/recipes?_app_id=#{app_id}&_app_key=#{app_key}&q=#{@search}&requirePictures=true"
+      encoded_url = URI.encode(url)
+      uri = URI("#{encoded_url}")
+      @results =  Net::HTTP.get(uri)
+  end
   end
 
   def show
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ActiveRecord::Base
   has_many :users, through: :favorites
   has_many :favorites
+
+#   def self.search_for
+#     @results = HTTParty.get(URI.escape("http://api.yummly.com/v1/api/recipes?_app_id=#{app_id}&_app_key=#{app_key}&q=#{@search}&requirePictures=true"
+# ))
+#   end
 end
